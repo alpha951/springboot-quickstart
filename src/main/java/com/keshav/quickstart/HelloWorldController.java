8bd403a48c740d9e9267b4fa627003b9f4fba41e
alpha951 <20uec068@lnmiit.ac.in>
2024-01-16 14:02:11 +0530

    First controller

---
+++ src/main/java/com/keshav/quickstart/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.keshav.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path = "/hello")
+    public String helloWorld() {
+        return "Hello World, Keshav!";
+    }
+}
